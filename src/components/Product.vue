0a8886dfa3d01cb91b65f722cd06bbf2e4898f9f
Paul Caruana <paul.caruana@leighton.com>
2021-02-17 15:55:25 +0000

    main app complete

---
+++ src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-2 m-auto">
+        <button @click="this.$emit('addToCart', item)" class="btn btn-success">
+            +
+        </button>
+    </div>
+    <div class="col-sm-4">
+        <img class="img-fluid d-block" :src="item.image" :alt="item.name" />
+    </div>
+    <div class="col">
+        <h3 class="text-primary">{{ item.name }}</h3>
+        <p class="mb-0">{{ item.description }}</p>
+        <div class="h5 float-right">
+            <span class="label"></span><curr :amt="item.price"></curr>
+        </div>
+    </div>
+</template>
+
+<script>
+import Curr from '@/components/Curr'
+export default {
+    props: ['item'],
+    emits: ['addToCart'],
+    components: {
+        Curr
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,31 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+// eslint-disable-next-line prettier/prettier
+import { createRouter, createWebHashHistory } from 'vue-router';
+// eslint-disable-next-line prettier/prettier
+import Home from '../views/Home.vue';
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+    // eslint-disable-next-line prettier/prettier
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
+    // eslint-disable-next-line prettier/prettier
 });
 
+// eslint-disable-next-line prettier/prettier
 export default router;
